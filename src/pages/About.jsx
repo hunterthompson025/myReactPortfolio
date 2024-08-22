e82a08d01aed8467a8e5da7ad11b544083ce3974
Hunter Thompson <hunterthompson025@gmail.com>
2024-08-21 19:50:11 -0700

    This commit adds my About Me page in the About.jsx file.

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I graduated from Arizona State University in 2017. I am currently
+        enrolled in the University of California Berkeley Extension Coding
+        Bootcamp. I have 7 years of experience in UI/UX testing at a software
+        company. Currently I am a lead of the Quality Assurance department as
+        well as a Scrum Master for one of the Agile teams with 2 QA and 6
+        Developers in the team. In my personal life I enjoy sports, travelling
+        and video games.
+      </p>
+    </div>
+  );
+}
