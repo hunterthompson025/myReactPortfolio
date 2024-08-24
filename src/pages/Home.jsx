166b975d5c6e45cfeac0c7de696a4d14cbb21dfd
Hunter Thompson <hunterthompson025@gmail.com>
2024-08-24 16:44:16 -0700

    This commit removes all the logic for the home page as it is not required. Also updates the logic to take you to the about me page when first loaded.

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <div className="vh-75 vw-75">
-            <h2 className="mb-3">Home</h2>
-            <p className="mb-3">Welcome to my portfolio!</p>
-            <img className="background" src='./src/assets/images/arizona.jpg' alt='picture of arizona desert' />
-        </div>
-    );
-}
